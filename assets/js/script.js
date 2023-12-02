2b6e924f194c568c5a89e7a1cdde025c463999bf
asif2212 <asifmd2212@gamil.com>
2023-12-02 15:43:33 +0600

    carousel added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 // slick carousel script
 $(document).ready(function(){
-    $('.demo').slick();
+    $('.demo').slick({
+        adaptiveHeight:true,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        dots:true,
+        dotsClass:'slick-dots',
+    });
+    $('.demoTwo').slick({
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:true,
+        // prev arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+    });
   });
